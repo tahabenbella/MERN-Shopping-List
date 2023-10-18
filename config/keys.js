bbd8f9c91f00d9b6695f6586a84fbe43200610c3
tahabenbella <117357515+tahabenbella@users.noreply.github.com>
2023-10-18 22:03:17 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports ={
-    mongoURI : "mongodb+srv://taha:12345-Adziko@cluster0.rxyrdmw.mongodb.net/?retryWrites=true&w=majority"
+    mongoURI : "mongodb+srv://taha:<password>@cluster0.rxyrdmw.mongodb.net/?retryWrites=true&w=majority"
 }
